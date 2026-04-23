3fa3c3d8884b37bb3b6bb0b1108c28bb8cb99a2f
hvcym <himanshuyadav20@hotmail.com>
2026-04-24 00:57:48 +0530

    Daily_Problem_24_April_2026

---
+++ LeetCode/2615. Sum of Distances.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+  vector<long long> distance(vector<int>& nums) {
+    int n = nums.size();
+    vector<long long> arr(n);
+    map<int, long long> sum;
+    unordered_map<int, int> cnt;
+    for (int i = 0; i < n; i++) {
+      arr[i] += 1ll * cnt[nums[i]] * i - sum[nums[i]];
+      sum[nums[i]] += i;
+      cnt[nums[i]] += 1;
+    }
+    sum.clear();
+    cnt.clear();
+    for (int i = n - 1; i >= 0; i--) {
+      arr[i] += sum[nums[i]] - 1ll * cnt[nums[i]] * i;
+      sum[nums[i]] += i;
+      cnt[nums[i]] += 1;
+    }
+    return arr;
+  }
+};
